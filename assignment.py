dd9e96ef429f096bc5c5619282563b0bb78749cf
Enrico Tabilin <enrico.tabilin@stu.ocsb.ca>
2024-11-26 15:34:35 +0000

    Submit 2

--- assignment.py
+++ assignment.py
@@ -1,11 +1,12 @@
 def calculatePi(digits):
   try:
     result = 0
-    if int(digits)<0:
+    if int(digits)<1:
       result = 'Invalid'
       return result
-    for n in range(int(digits)+1):
-      result = result + 4*((-1)**n/(2*n+1))
+    for n in range(1,int(digits)+1):
+      n=n-1
+      result = result + 4*((-1.0)**n/(2.0*n+1.0))
   except:
     result = 'Invalid'
   return result
