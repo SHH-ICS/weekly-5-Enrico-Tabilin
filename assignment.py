4dd9a917b55c2d7eb074038080be16c63e866ab5
Enrico Tabilin <enrico.tabilin@stu.ocsb.ca>
2024-11-26 15:23:02 +0000

    Submit 1

--- assignment.py
+++ assignment.py
@@ -1,5 +1,13 @@
 def calculatePi(digits):
-  result = 0
+  try:
+    result = 0
+    if int(digits)<0:
+      result = 'Invalid'
+      return result
+    for n in range(int(digits)+1):
+      result = result + 4*((-1)**n/(2*n+1))
+  except:
+    result = 'Invalid'
   return result
 
 number_of_digits = input()
