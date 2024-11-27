0c32767670c0812210987676dd652ca392751771
Enrico Tabilin <enrico.tabilin@stu.ocsb.ca>
2024-11-27 15:19:37 +0000

    Update index.html and handler.php

--- assignment.py
+++ assignment.py
@@ -6,7 +6,7 @@ def calculatePi(digits):
       return result
     for n in range(1,int(digits)+1):
       n=n-1
-      result = result + 4*((-1.0)**n/(2.0*n+1.0))
+      result += 4*((-1.0)**n/(2.0*n+1.0))
   except:
     result = 'Invalid'
   return result
